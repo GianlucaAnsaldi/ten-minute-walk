52bed680805806d47106797cc0c24ff2410a6975
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-29 15:53:04 +0100

    initial refactor

--- src/tenMinuteWalk.js
+++ src/tenMinuteWalk.js
@@ -2,17 +2,17 @@ const tenMinuteWalk = (directions) => {
   let vertical = 0,
     horizontal = 0;
 
-  if (directions.length != 10) {
-    return false;
-  } else {
+  if (directions.length === 10) {
     for (let direction of directions) {
       if (direction == 'n') vertical += 1;
       if (direction == 's') vertical -= 1;
       if (direction == 'w') horizontal += 1;
       if (direction == 'e') horizontal -= 1;
     }
+    return vertical === 0 && horizontal === 0;
+  } else {
+    return false;
   }
-  return vertical === 0 && horizontal === 0;
 };
 
 module.exports = tenMinuteWalk;
